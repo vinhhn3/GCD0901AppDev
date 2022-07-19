1704932f70b4d0e61e7bf182ff526ebbf91dcb3b
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-07-19 16:27:59 +0700

    Add project files.

---
+++ GCD0901AppDev/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GCD0901AppDev.Data
+{
+  public class ApplicationDbContext : IdentityDbContext
+  {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+  }
+}
---
+++ GCD0901AppDev/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GCD0901AppDev.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
