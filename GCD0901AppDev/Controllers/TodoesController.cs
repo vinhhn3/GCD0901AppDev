6ee4e6c0befa602167f17621389d08847f104b24
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-07-22 08:59:34 +0700

    Create TodoesController and View

---
+++ GCD0901AppDev/Controllers/TodoesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GCD0901AppDev.Controllers
+{
+  public class TodoesController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
