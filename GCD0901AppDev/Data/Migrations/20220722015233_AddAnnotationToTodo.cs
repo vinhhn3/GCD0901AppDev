9630b5f4ac21dd14c692335b42035cf9d44538c5
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-07-22 08:53:37 +0700

    Update Todo class

---
+++ GCD0901AppDev/Data/Migrations/20220722015233_AddAnnotationToTodo.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GCD0901AppDev.Data.Migrations
+{
+    public partial class AddAnnotationToTodo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Todoes",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Todoes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+        }
+    }
+}
--- GCD0901AppDev/Models/Todo.cs
+++ GCD0901AppDev/Models/Todo.cs
@@ -1,12 +1,16 @@
 ﻿using GCD0901AppDev.Enums;
 
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GCD0901AppDev.Models
 {
   public class Todo
   {
+    [Key]
     public int Id { get; set; }
+    [Required]
+    [StringLength(255)]
     public string Description { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public TodoStatus Status { get; set; } = TodoStatus.Todo;
