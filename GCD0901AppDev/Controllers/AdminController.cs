2798b84754bf86b939186f584922d4147700dcec
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-07-29 11:31:30 +0700

    Custom Home page for Admin

---
+++ GCD0901AppDev/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using GCD0901AppDev.Utils;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GCD0901AppDev.Controllers
+{
+  [Authorize(Roles = Role.ADMIN)]
+  public class AdminController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
