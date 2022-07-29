2eb6f2e08d466bbf12dd551152db56497788c9d0
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-07-29 09:54:44 +0700

    Seeding Roles to database

--- GCD0901AppDev/Data/ApplicationDbContext.cs
+++ GCD0901AppDev/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using GCD0901AppDev.Models;
+using GCD0901AppDev.Utils;
 
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,7 +14,22 @@ namespace GCD0901AppDev.Data
     {
     }
 
+
     public DbSet<Todo> Todoes { get; set; }
     public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      base.OnModelCreating(builder);
+      this.SeedRoles(builder);
+    }
+
+    private void SeedRoles(ModelBuilder builder)
+    {
+      builder.Entity<IdentityRole>().HasData(
+          new IdentityRole() { Id = "fab4fac1-c546-41de-aebc-a14da6895711", Name = Role.USER, ConcurrencyStamp = "1", NormalizedName = Role.USER },
+          new IdentityRole() { Id = "c7b013f0-5201-4317-abd8-c211f91b7330", Name = Role.ADMIN, ConcurrencyStamp = "2", NormalizedName = Role.ADMIN }
+          );
+    }
   }
 }
---
+++ GCD0901AppDev/Data/Migrations/20220729025149_AddRoles.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GCD0901AppDev.Data.Migrations
+{
+    public partial class AddRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "fab4fac1-c546-41de-aebc-a14da6895711", "1", "user", "user" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "c7b013f0-5201-4317-abd8-c211f91b7330", "2", "admin", "admin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c7b013f0-5201-4317-abd8-c211f91b7330");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "fab4fac1-c546-41de-aebc-a14da6895711");
+        }
+    }
+}
