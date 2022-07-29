370866e525fcadf0e06ee5fa0eeca10793fb5374
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-07-29 08:51:44 +0700

    Custom Register view

---
+++ GCD0901AppDev/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GCD0901AppDev.Data;
+using GCD0901AppDev.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GCD0901AppDev.Areas.Identity.IdentityHostingStartup))]
+namespace GCD0901AppDev.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ GCD0901AppDev/Utils/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace GCD0901AppDev.Utils
+{
+  public class Role
+  {
+    public const string ADMIN = "admin";
+    public const string USER = "user";
+  }
+}
