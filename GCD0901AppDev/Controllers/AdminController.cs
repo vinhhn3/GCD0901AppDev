7b90b374229e137c69e4dea243be0040d8998ebd
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-08-02 13:57:08 +0700

    Admin can see all Users accounts

--- GCD0901AppDev/Controllers/AdminController.cs
+++ GCD0901AppDev/Controllers/AdminController.cs
@@ -1,6 +1,9 @@
-﻿using GCD0901AppDev.Utils;
+﻿using GCD0901AppDev.Data;
+using GCD0901AppDev.Models;
+using GCD0901AppDev.Utils;
 
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GCD0901AppDev.Controllers
@@ -8,9 +11,26 @@ namespace GCD0901AppDev.Controllers
   [Authorize(Roles = Role.ADMIN)]
   public class AdminController : Controller
   {
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    private readonly ApplicationDbContext _context;
+
+    public AdminController(
+      UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+    {
+      _userManager = userManager;
+      _context = context;
+    }
+
     public IActionResult Index()
     {
       return View();
+    }
+    [HttpGet]
+    public IActionResult Users()
+    {
+      var usersWithPermission = _userManager.GetUsersInRoleAsync(Role.USER).Result;
+      return View(usersWithPermission);
     }
   }
 }
---
+++ GCD0901AppDev/ViewModels/UsersViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GCD0901AppDev.ViewModels
+{
+  public class UsersViewModel
+  {
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
+  }
+}
