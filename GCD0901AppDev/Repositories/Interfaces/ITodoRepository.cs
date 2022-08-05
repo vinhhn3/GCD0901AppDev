aec5ffdaea0b5bbd690ba5005210b7b596e5748f
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-08-05 08:06:52 +0700

    Create ITodoRepository

---
+++ GCD0901AppDev/Repositories/Interfaces/ITodoRepository.cs
@@ -0,0 +1,17 @@
+﻿using GCD0901AppDev.Models;
+using GCD0901AppDev.ViewModels;
+
+using System.Collections.Generic;
+
+namespace GCD0901AppDev.Repositories.Interfaces
+{
+  public interface ITodoRepository
+  {
+    IEnumerable<Todo> GetAll();
+    bool CreateTodo(TodoCategoriesViewModel viewModel);
+    bool DeleteById(int id);
+    Todo GetById(int id);
+    bool EditTodo(TodoCategoriesViewModel viewModel);
+    IEnumerable<Todo> GetTodoesByCategoryId(int id);
+  }
+}
