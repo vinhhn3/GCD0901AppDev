51a96e510a887a7078df2ffbfa114353ce1a28ae
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-08-05 11:34:12 +0700

    More ...

--- GCD0901AppDev/Repositories/Interfaces/ITodoRepository.cs
+++ GCD0901AppDev/Repositories/Interfaces/ITodoRepository.cs
@@ -13,10 +13,10 @@ namespace GCD0901AppDev.Repositories.Interfaces
     IEnumerable<Todo> GetAll(string userId, string categoryName);
     IEnumerable<Todo> GetAllByCategoryId(int id);
 
-    Task<bool> CreateTodo(CreateTodoRequest viewModel, string userId);
+    Task<bool> CreateTodo(CreateTodoRequest model, string userId);
     bool DeleteTodo(int id, string userId);
     Todo GetTodo(int id);
     Todo GetTodo(int id, string userId);
-    bool EditTodo(EditTodoRequest viewModel, string userId);
+    bool EditTodo(EditTodoRequest model, string userId);
   }
 }
